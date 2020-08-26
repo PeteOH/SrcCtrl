253b6ccdd60f62e9a1f00f1903c774fbe5588d2c
PeteOH <peteohalloran@gmail.com>
2020-08-26 14:03:53 +1000

    AUTO UPDATE 2020/08/26 14:03:52

--- update.bat
+++ update.bat
@@ -3,7 +3,7 @@
 echo ================================================
 echo == Updating Github repository from IRIS
 echo ==
-echo ==    exportng IRIS packages
+echo ==    exportng IRIS packages...
 c:\intersystems\iris\bin\iristerm /console=cn_ap:iris C:\DEV\SrcCtrl\runexport.scr DEV C:\DEV\SrcCtrl CTRL
 echo ==
 echo ==    pushing to github...
